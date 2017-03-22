8d59a9454494faace797d606f79e59be4e341bcc
Stan Bershadskiy <stan@moduscreate.com>
2017-03-22 16:40:55 -0400

    giving android some love

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './js'
+import { AppRegistry } from "react-native";
+import App from "./js";
 
 AppRegistry.registerComponent("NCAPRNRedux", () => App);
